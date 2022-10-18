4eec8bc040ec1680315c981e4d835385f15a9432
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2022-10-18 12:37:59 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -265,8 +265,8 @@ async def delallconfirm(client, message):
         await message.reply_text(
             f"This will delete all filters from '{title}'.\nDo you want to continue??",
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="𝙔𝙀𝙎",callback_data="delallconfirm")],
-                [InlineKeyboardButton(text="𝘾𝘼𝙉𝘾𝙀𝙇",callback_data="delallcancel")]
+                [InlineKeyboardButton(text="𝗬𝗘𝗦",callback_data="delallconfirm")],
+                [InlineKeyboardButton(text="𝗖𝗔𝗡𝗖𝗘𝗟",callback_data="delallcancel")]
             ]),
             quote=True
         )
